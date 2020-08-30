417ad3fd1ce29308bec22e1c129fa48fa3bc529d
Adib Ghannam <m.adib.ghannam@gmail.com>
2020-08-31 03:16:55 +0800

    First Version

--- src/com/ayoush/Main.java
+++ src/com/ayoush/Main.java
@@ -3,7 +3,6 @@ package com.ayoush;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Aisha AlHarweel");
         System.out.println("I Love You");
     }
