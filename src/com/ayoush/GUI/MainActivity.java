107f1cde8e045b3da712f3204dbaad9d79a3b4fb
Adib Ghannam <m.adib.ghannam@gmail.com>
2020-08-31 03:56:55 +0800

    Created The Gui Package

---
+++ src/com/ayoush/GUI/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ayoush.GUI;
+
+import javax.swing.*;
+
+public class MainActivity {
+    public void run() {
+        JFrame jFrame = new JFrame("Smart Pharmacy");
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setSize(800, 600);
+    }
+
+
+}
--- src/com/ayoush/Main.java
+++ src/com/ayoush/Main.java
@@ -1,9 +1,9 @@
 package com.ayoush;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Aisha AlHarweel");
-        System.out.println("I Love You");
+
     }
 }
