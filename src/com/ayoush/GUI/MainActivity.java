dca9bfa00590a4bc67362d27470616ccb8a3e44b
Adib Ghannam <m.adib.ghannam@gmail.com>
2020-09-26 19:40:45 +0800

    Created The Patient Class and Age Setter

--- src/com/ayoush/GUI/MainActivity.java
+++ src/com/ayoush/GUI/MainActivity.java
@@ -3,11 +3,14 @@ package com.ayoush.GUI;
 import javax.swing.*;
 
 public class MainActivity {
-    public void run() {
+    private void run() {
         JFrame jFrame = new JFrame("Smart Pharmacy");
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setSize(800, 600);
+        jFrame.setVisible(true);
     }
+
+
 
 
 }
--- src/com/ayoush/Main.java
+++ src/com/ayoush/Main.java
@@ -2,8 +2,8 @@ package com.ayoush;
 
 
 public class Main {
-
     public static void main(String[] args) {
-
+        Patient patent123 = new Patient("Aisha", "AlHarweel", "Female", 31, 1, 2000);
+        System.out.println("Age: " + patent123.getAgeYears() + " Years and " + patent123.getAgeMonths() + " months");
     }
 }
