5e3c4f0e308a82b84f96bc6af3a1f63081cad8d8
Adib Ghannam <m.adib.ghannam@gmail.com>
2020-10-02 18:13:09 +0800

    added the Session Class

---
+++ Gradle/src/main/java/smart/BackEnd/Drug.java
@@ -0,0 +1,9 @@
+package smart.BackEnd;
+
+public class Drug {
+    private String drugName;
+    private String drugEffective;
+    private double dosage;
+    private int repetition;
+    private int repetitionUsed;
+}
---
+++ Gradle/src/main/java/smart/BackEnd/Session.java
@@ -0,0 +1,12 @@
+package smart.BackEnd;
+
+import java.util.ArrayList;
+
+public class Session {
+    private String medicalCondition;
+    private String doctorsName;
+    private String sessionTimeStamp;
+    private String sessionID;
+    private ArrayList<Drug> drugs = new ArrayList<>();
+    private String getDoctorsNotes;
+}
